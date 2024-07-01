24a4d0e921a8d552b6ed17e9b1ab94846c103a54
Jan Kraemer <jan.kraemer@vector.com>
2024-07-01 09:27:12 +0200

    fixup! docker: add Ubuntu 22.04 image and tests

--- utils/image_tests.sh
+++ utils/image_tests.sh
@@ -4,7 +4,7 @@ CMAKE_PRESET="gcc${1}-relwithdebinfo"
 
 echo "Building ${CMAKE_PRESET}"
 
-git -c http.sslverify=false clone --recursive --shallow-submodules --depth=1 https://github.com/vectorgrp/sil-kit.git
+git -c http.sslverify=false clone --recursive --shallow-submodules --depth=1 -b add_generic_relwithdebinfo https://github.com/vjankraemer/sil-kit-ci.git sil-kit
 
 cd sil-kit
 
